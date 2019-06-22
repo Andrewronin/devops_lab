8256c5ff559dcdaf20c7a028d049ca4604860a06
AndRoniN <andrewronin@mail.ru>
2019-06-22 23:19:25 +0300

    hometask3: script and whl

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-DevOps Lab 2019 (May-August)
+ 
+It's a simple python app whish monitor your computer.
+Output should be written to ./output/output.txt or ./output/output.json
+Configuration can be changed in ./config.json
+Class description you can find in ./my_class.py
+To start monitoring open ./main.py or start with 'python snapshot'
+
+Libraries: 
+psutil -- for monitoring
+prettytable -- for txt.output
+
+
---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from my_class import Snapshot
+import json
+import time
+import os
+
+
+def main():
+    # Reading config
+
+    ospath = os.path.abspath(os.path.dirname(__file__))
+    config_path = os.path.join(ospath, 'config.json')
+
+    with open(config_path, "r") as file:
+        config = json.load(file)
+
+    snap = Snapshot()
+
+    # Endless while
+    while True:
+        snap.make()
+        snap.output(ospath, config.get("output"))
+        print("Snapshot was created")
+        time.sleep(int(config.get("interval")))
+
+
+if __name__ == "__main__":
+    main()
